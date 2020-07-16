777c5067187a2bee73e139deb09abcfd27f21fc0
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-07-16 11:18:29 -0400

    solution

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 // Complete the staircase function below.
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
 function staircase(n) {
 
 for ( let i = 1; i <= n; i++ ) {
@@ -7,6 +32,13 @@ for ( let i = 1; i <= n; i++ ) {
 
 }
 
-staircase(4);
+// staircase(4);
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
+
 
 // A single integer, n, denoting the size of the staircase. 
